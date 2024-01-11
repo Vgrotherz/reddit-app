e9c24df99ec58555c2b9146cc80a86810ed231a1
Vgrotherz <vgrotherz@gmail.com>
2024-01-11 18:30:43 +0700

    add header component and starting search function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
 import './App.css';
+
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,7 +51,8 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
+      <Header />
     </div>
   );
 }
